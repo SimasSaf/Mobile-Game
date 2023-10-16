870a6050c8111ba95c4ec3ff611cc480a600939f
Simas <simas.safr@gmail.com>
2023-10-16 18:52:44 +0300

    added material

--- app/src/main/java/com/idlegame/MainActivity.kt
+++ app/src/main/java/com/idlegame/MainActivity.kt
@@ -2,10 +2,16 @@ package com.idlegame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
